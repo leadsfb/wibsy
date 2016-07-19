d0cecf1f49f7a792b25f9fed53466bd7b33bcb4b
ayoub <ayoub@wib.me>
2016-07-19 18:03:59 +0100

    webhook

--- webhook.php
+++ webhook.php
@@ -1,12 +1,13 @@
 <?php
 
-$challenge=$_REQUEST['hub_challenge'];
 $verify_token=$_REQUEST['hub_verify_token'];
 
 if($verify_token=="abc123"){
-echo challenge
-}
+echo isset( $_REQUEST['hub_challenge'] ) ? $_REQUEST['hub_challenge'] : false;
+};
 
-
+$inputJSON = file_get_contents('php://input');
+$input = json_decode($inputJSON, true);
+error_log(print_r($input, true));
 
 ?>
